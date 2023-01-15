3c4556068d89f773b4cde5e59aabcef1054dbbca
Luis Höfer <luis.hoefer@gmail.com>
2023-01-15 10:32:47 +0100

    feat: format commands in table

--- src/utils/commands.format.ts
+++ src/utils/commands.format.ts
@@ -1,3 +1,5 @@
+import { table, TableUserConfig } from 'table';
+
 interface CommandOutput {
   description?: string;
   alias?: string;
@@ -7,8 +9,27 @@ interface CommandOutput {
  * Format command output
  */
 export const formatCommands = (commands: { [key: string]: CommandOutput }) => {
-  console.log('Commands');
-  Object.entries(commands).forEach(([key, value]) => {
-    console.log('$', key, '::', value.description || value.alias);
+  const tableData = convertData(commands);
+  console.log(table(tableData, tableConfig));
+};
+
+/**
+ * Convert commands json to array for table output
+ *
+ * @param json
+ * @returns
+ */
+const convertData = (json: any) => {
+  const arr = [];
+  arr.push(['Commands', '', '']);
+  arr.push(['Command', 'Description', 'Alias']);
+  Object.entries(json).forEach(([key, value]: any) => {
+    arr.push([key, value.description, value.alias]);
   });
+  return arr;
+};
+
+const tableConfig: TableUserConfig = {
+  // columns: [{ alignment: 'center' }],
+  spanningCells: [{ col: 0, row: 0, colSpan: 3 }],
 };
